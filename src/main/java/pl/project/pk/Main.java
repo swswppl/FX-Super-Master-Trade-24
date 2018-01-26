866509dff9cc51779140cd2647c5d852b83969ce
Patryk Przybek <patryk.przybek@interia.pl>
2018-01-26 17:19:50 +0100

    poprawki errorow okienek

--- src/main/java/pl/project/pk/Main.java
+++ src/main/java/pl/project/pk/Main.java
@@ -1,19 +1,14 @@
 package pl.project.pk;
 
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import pl.project.pk.utils.FxmlUtils;
 
-import java.util.ResourceBundle;
-
 public class Main extends Application {
 
-    public static final String BORDER_PANE_MAIN_FXML = "/fxml/BorderPaneMain.fxml";
+    private static final String BORDER_PANE_MAIN_FXML = "/fxml/BorderPaneMain.fxml";
 
     public static void main(String[] args) {
         launch(args);
